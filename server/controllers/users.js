2b1f0ea008fab4b7aab99b4dcf0066b18117470e
Lilia <lilia.manukyan@hotmail.com>
2021-02-22 17:45:18 +0400

    jwt auth backend for sign in and sign up

---
+++ server/controllers/users.js
@@ -0,0 +1,60 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({ message: "The email is not registered." });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordCorrect) {
+      return res.status(404).json({ message: "Invalid password." });
+    }
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      "test",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const signup = async (req, res) => {
+  const { firstName, lastName, email, password, confirmPassword } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists." });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords don't match." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      name: `${firstName} ${lastName}`,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign({ email: newUser.email, id: newUser._id }, "test", {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ result: newUser, token });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
